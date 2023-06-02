40d5fd4de2b59874e364e206222320547924bdac
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-06-02 19:53:26 +0300

    Summary-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ulEl = document.querySelector('.gallery');
 
 console.log(galleryItems);
+
+function createGalleryElement(items) {
+    let galleryLi = document.createElement('li');
+    galleryLi.classList.add('gallery__item');
+    
+    let galleryElements = items.map(item => {
+        let itemLink = document.createElement('a');
+        itemLink.classList.add('gallery__link');
+        itemLink.href = item.original; 
+
+        let imgEl = document.createElement('img');
+        imgEl.classList.add('gallery__image');
+
+        imgEl.dataset.source = item.original;
+        imgEl.src = item.preview;
+        imgEl.alt = item.description;
+        itemLink.appendChild(imgEl);
+
+        return itemLink;
+    });
+
+    console.log(galleryElements);
+    galleryLi.append(...galleryElements)
+    ulEl.append(galleryLi);
+
+
+    return ulEl;
+}
+
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    let clickedItemLi = event.target.closest('.gallery__item');
+    console.log(clickedItemLi);
+    if (!clickedItemLi) {
+        return;
+    }
+    
+  const instance = basicLightbox.create(`
+    <div class="modal"><img src="${event.target.dataset.source}" >
+    </div>
+`)
+
+  instance.show();
+  
+  ulEl.addEventListener('keydown', (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function bindEvents(ulEl) {
+    ulEl.addEventListener('click', onGalleryItemClick)
+}
+
+function initGallery(items) {
+    let galleryLi = createGalleryElement(items);
+   
+    bindEvents(ulEl);
+   
+}
+
+initGallery(galleryItems);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const ulEl = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => {
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    return markup;
+})
+
+ulEl.insertAdjacentElement('beforeend', markup.join(''));
+
+// function createGalleryElement(items) {
+//     let galleryLi = document.createElement('li');
+//     galleryLi.classList.add('gallery__item');
+    
+//     let galleryElements = items.map(item => {
+//         let itemLink = document.createElement('a');
+//         itemLink.classList.add('gallery__link');
+//         itemLink.href = item.original; 
+
+//         let imgEl = document.createElement('img');
+//         imgEl.classList.add('gallery__image');
+
+//         
+//         imgEl.src = item.preview;
+//         imgEl.alt = item.description;
+//         itemLink.appendChild(imgEl);
+
+//         return itemLink;
+//     });
+
+//     console.log(galleryElements);
+//     galleryLi.append(...galleryElements)
+//     ulEl.append(galleryLi);
+
+
+//     return ulEl;
+// }
+
+
+
+{/* <li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li> */}
